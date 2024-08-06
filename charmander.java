17540984b392588c2583275608fd2661ea2aea02
Adam <adamplesca1@gmail.com>
2024-08-06 15:57:59 +0100

    added base stats

--- charmander.java
+++ charmander.java
@@ -1,3 +1,15 @@
 public class charmander {
-    
+
+        //pokemon stats
+        String type = "Fire";
+        int hp = 20;
+        int attack = 11;
+        int specAtt = 12;
+        int defense = 10;
+        int specDef = 13;
+        int speed = 10;
+
+        //moves
+
+        
 }
--- squirtle.java
+++ squirtle.java
@@ -1,3 +1,13 @@
 public class squirtle {
-    
+
+        //pokemon stats
+        String type = "Water";
+        int hp = 20;
+        int attack = 11;
+        int specAtt = 12;
+        int defense = 10;
+        int specDef = 13;
+        int speed = 10;
+
+        //moves
 }
