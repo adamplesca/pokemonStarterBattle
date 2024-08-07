b9fc5d707ac784256593d67bea2682c54b0c3a8b
Adam <adamplesca1@gmail.com>
2024-08-07 23:35:56 +0100

    imports + info

--- pokemonBattleApp.java
+++ pokemonBattleApp.java
@@ -1,6 +1,20 @@
+import javax.swing.JOptionPane;
+
 public class pokemonBattleApp{
     public static void main(String [] args){
 
-        
+        JOptionPane.showMessageDialog(null, "                                   Welcome to Pokemon Battle Sim" 
+                                                        + "\n" 
+                                                        + "You will be choosing a starter Pokemon in which you will fight your rival 'Blue'.");
+
+        JOptionPane.showMessageDialog(null, "                                   In case you don't know the rules, Here they are" 
+                                                        + "\n"
+                                                        + "Your objective is faint your rivals pokemon,"
+                                                        + "\n"
+                                                        + "You do this by dealing damage to them until they no longer have any HP ('HitPoints'),"
+                                                        + "\n"
+                                                        + "You deal damage through attacks that your pokemon of choice has,"
+                                                        + "\n"
+                                                        + "Goodluck");
     }
 }
