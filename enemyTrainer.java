9f3302508d9158fdac89b3814c4f5defed7b1b1c
Adam Plesca <116078864+adamplesca@users.noreply.github.com>
2024-08-10 20:43:25 +0100

    start of pokemon moveChoice, feat(Dom)

--- enemyTrainer.java
+++ enemyTrainer.java
@@ -1,6 +1,17 @@
 public class enemyTrainer {
     
-    //make so wherever user choose, rival will choose stronger pokemon typing
+//make so wherever user choose, rival will choose stronger pokemon typing
 
-    
+   private String pokemon;
+   //private String 
+
+   public void outcome(String chosenPokemon){
+    pokemon = chosenPokemon;
+   } 
+
+   public 
+   if(chosenPokemon = "bulbasaur"){
+    charmander charmanderPokemon = new charmander();
+   }
+
 }
