e256a9abd5dfc97283c6317598a2bdc28260de89
Adam Plesca <116078864+adamplesca@users.noreply.github.com>
2024-08-10 20:44:10 +0100

    added code, feat(Dom + Hamilton)

--- mainLogic.java
+++ mainLogic.java
@@ -1,3 +1,27 @@
 public class mainLogic {
     
+    bulbasaur p1 = new bulbasaur();
+    charmander p2 = new charmander();
+    squirtle p3 = new squirtle();
+
+    //bulbasaur attacks
+    int bulTackle = p1.tackle();
+    int bulVineWhip = p1.vineWhip();
+
+    
+    String name = "asdi " + "dsaoind";
+    
+
+    public int returnHP(){
+        return p1.hp;
+    }
+
+    public String returnType(){
+        return p1.type;
+    }
+
+    public int attackDamage(){
+        return p2.hp - damage;
+    }
+
 }
