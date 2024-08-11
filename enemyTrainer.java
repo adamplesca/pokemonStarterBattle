8b27bd1776de2365e7452bc1db093e85099fa95d
Adam <adamplesca1@gmail.com>
2024-08-11 17:56:48 +0100

    added pokemon choice

--- enemyTrainer.java
+++ enemyTrainer.java
@@ -1,17 +1,45 @@
+
 public class enemyTrainer {
     
-//make so wherever user choose, rival will choose stronger pokemon typing
-
-   private String pokemon;
-   //private String 
+   private String pokemon, message;
 
    public void outcome(String chosenPokemon){
     pokemon = chosenPokemon;
    } 
 
-   public 
-   if(chosenPokemon = "bulbasaur"){
-    charmander charmanderPokemon = new charmander();
+   //rival chooses pokemon
+   public String rivalPokemonChoice(){
+       switch (pokemon) {
+           case "Bulbasaur" -> {
+               message = "Your rival chose 'Charmander'!";   
+               charmander chaPokemon = new charmander();
+               //JOptionPane.showMessageDialog(null, "Your rival 'Blue' picked 'Charmander'!");
+           }
+           case "Charmander" -> {
+               message = "Your rival chose 'Squirtle'!";
+               squirtle squPokemon = new squirtle();
+               //JOptionPane.showMessageDialog(null, "Your rival 'Blue' picked 'Squirtle'!");
+           }
+           case "Squirtle" -> {
+            message = "Your rival chose 'Bulbasaur'!";
+            bulbasaur bulPokemon = new bulbasaur();
+            //JOptionPane.showMessageDialog(null, "Your rival 'Blue' picked 'Bulbasaur'!");
+           }
+           default -> {
+            message = "Error, did not choose pokemon";
+           }
+       }
+       return message;
    }
 
+   //rival move selector for each pokemon
+   public void rivalPokemonMoveForBulbasaur(){
+
+   }
+   public void rivalPokemonMoveForCharmander(){
+
+   }
+   public void rivalPokemonMoveForSquirtle(){
+
+   }
 }
